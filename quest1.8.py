385b63114d19781609541c38bbab6fb5ab3ab748
prajwal acharya <callmeprajwal@gmail.com>
2017-05-11 18:49:05 -0700

    Chapter 1 completed

--- quest1.8.py
+++ quest1.8.py
@@ -1,22 +1,10 @@
-def setrowzero(matrix):
-    m=len(matrix)
-    n=len(matrix[0])
-    xoffset=[]
-    yoffset=[]
-    for i in range(0,m):
-        for j in range(0,n):
-            if matrix[i][j]==0:
-                if i not in xoffset:
-                    xoffset.append(i)
-                if j not in yoffset:
-                    yoffset.append(j)
+def isrotation(str1, str2):
+    str3 = ''.join([str1,str1])
 
-    for i in range(0,m):
-        for j in range(0,n):
-            if i in xoffset or j in yoffset:
-                matrix[i][j]=0
+    if str2 in str3:
+        return True
+    else:
+        return False
 
-    return matrix
 
-matrix=[[1,2,3],[4,0,6],[7,8,9]]
-print setrowzero(matrix)
+print isrotation("myname", "myr")
